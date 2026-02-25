510dabbaac42c9c7bb5dffb818f7261ee7f9f5fe
Sean Devlin <sean.dev224@gmail.com>
2026-02-24 19:41:04 -0500

    solenoid code ported to platform io

--- include/knob_servo.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <Arduino.h>
-#include <Servo.h>
-
-void initKnobServo(int servoPin);
-void spinKnobServo(int pulse);
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,52 @@
 #include <Arduino.h>
-#include "knob_servo.h"
 
-#define KNOB_PIN 2
+/*
+About: Made for the 2026 IEEE Competition keypad task. 
+To accomplish this task, solenoids are used to enter the code 73738# (RESET#)
+Version: October 7th, 2025
+*/
 
-void setup(){
-  initKnobServo(KNOB_PIN);
+// Defined Pins:
+
+int solenoid3 = 3;
+int solenoid7 = 2;
+int solenoid8 = 4;
+int solenoidPound = 5;
+int delayAfterHigh= 100;
+int delayAfterLow = 2000;
+int soleArr[6] = {solenoid7, solenoid3, solenoid7, solenoid3, solenoid8, solenoidPound};
+int startDelay = 3000;
+
+void setup() {
+  // Pins set as output:
+
+  pinMode(solenoid7, OUTPUT);
+  pinMode(solenoid3, OUTPUT);
+  pinMode(solenoid8, OUTPUT);
+  pinMode(solenoidPound, OUTPUT);
+
+  // Solenoid sequence runs:
+  // Code: 73738# (RESET#)
+
+  delay(startDelay);
+
+  
 }
 
-void loop(){
-  delay(5000);
-  spinKnobServo(2000);
-  delay(5000);
+void funcInputter(int num)
+{
+  digitalWrite(num, HIGH);
+  delay(delayAfterHigh);
+  digitalWrite(num, LOW);
+  delay(delayAfterLow);
 }
 
+void loop() {
+  // Prototype Optimization 2
+  for (int i = 0; i<6; i++)
+  {
+    funcInputter(soleArr[i]);
+  }
+
+  delay(5000);
+}
