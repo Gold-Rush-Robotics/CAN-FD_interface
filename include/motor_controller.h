db983427133a1640413bab2bcea0c9f070064a6e
Anthony DiBernardo <adibern2@charlotte.edu>
2026-03-10 15:14:08 -0400

    Revert "combine #1"
    This reverts commit 4a2fd821b332199ad68cdb78b6c03cf418eee318, reversing
    changes made to 87cc68351aa690ae6c910052e1f1bfb51c1e2bf8.

---
+++ include/motor_controller.h
@@ -0,0 +1,26 @@
+#ifndef MOTOR_CONTROLLER_H
+#define MOTOR_CONTROLLER_H
+
+#include <Arduino.h>
+#include <Encoder.h>
+
+class MotorController {
+public:
+  MotorController(int dirPin, int pwmPin, int slpPin, int fltPin, int encA, int encB, int csPin, int starting_direction = 1);
+  bool begin();
+  void setSpeed(int pwmVal);
+  void setSpeedRPM(float rpm);
+  float getRPM();
+  int getFaultPin();
+
+private:
+  int _dirPin, _pwmPin, _slpPin, _fltPin, _encA, _encB, _csPin;
+  Encoder* _encoder;
+  long _lastEncoderCount = 0;
+  unsigned long _lastTime = 0;
+  const int _ticksPerRev = 100; // Adjust as needed
+  const float _gearRatio = 1.0; // Adjust as needed
+  int starting_direction = 1; // 1 for forward, -1 for reverse
+};
+
+#endif
